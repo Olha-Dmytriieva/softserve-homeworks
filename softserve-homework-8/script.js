a1551122077362c48362998ac149854615064ff9
Olha-Dmytriieva <tarakanola@gmail.com>
2021-02-22 21:42:22 +0200

    corrected hw 8 according to menthor`s remark

--- softserve-homework-8/script.js
+++ softserve-homework-8/script.js
@@ -1,105 +1,103 @@
 // //task1//
 
 
-// function upperCase(expression){
+function upperCase(expression){
 
-//     if(expression.match(/[A-Z]/)){
-//         console.log("String's starts with uppercase character")
-//     } else if(expression.match(/[A-Z]/) === null){
-//          console.log("String's not start with uppercase character")
-//     }
+    if(expression.match(/[A-Z]/)){
+        console.log("String's starts with uppercase character")
+    } else if(expression.match(/[A-Z]/) === null){
+         console.log("String's not start with uppercase character")
+    }
 
     
 
-// }
+}
 
-// upperCase('regexp');
-// upperCase('RegExp');
+upperCase('regexp');
+upperCase('RegExp');
 
 
 
 // //task2//
 
-// function checkEmail(mail){
+function checkEmail(mail){
 
-//     const checkExp = mail.match((/\@/ig))
+    const checkExp = mail.match((/\@/ig))
 
-//     if(mail.match((/[\!\&\^\%\$\*\(\)\+\>\<\[\]\{\}\?\:\;\,\`\~\#\"\'\/\|]/ig))){
-//         console.log('false')
-//     } else if (checkExp.length === 1 && mail.match((/[\.\-\_]/ig))){
-//         console.log('true')
-//     }
+    if(mail.match((/[\!\&\^\%\$\*\(\)\+\>\<\[\]\{\}\?\:\;\,\`\~\#\"\'\/\|]/ig))){
+        console.log('false')
+    } else if (checkExp.length === 1 && mail.match((/[\.\-\_]/ig))){
+        console.log('true')
+    }
    
         
-// }
+}
 
-// checkEmail("Qmail2@gmail.com")
-// checkEmail("lalala18181@mail.com")
+checkEmail("Qmail2@gmail.com")
+checkEmail("lalala18181@mail.com")
 
 
 // //task3//
 
 
-// console.log("cdbBdbsbz".match(/d(b{1,})(d{1,})/i))
+console.log("cdbBdbsbz".match(/d(b{1,})(d{1,})/i))
 
-// console.log((/d(b{1,})(d{1,})/i).exec("cdbBdbsbz"))
+console.log((/d(b{1,})(d{1,})/i).exec("cdbBdbsbz"))
 
 
 
 
 // //task4//
 
-// const string = "Java Script";
-// let pattern = /\s/;
-// let arr = []
+const string = "Java Script";
+let pattern = /\s/;
+let arr = []
 
-// arr.push(string.split(pattern).reverse([0,1]))
-// console.table(arr)
+arr.push(string.split(pattern).reverse([0,1]))
+console.table(arr)
 
  
 
 // //task5//
 
-// function ccValidation(cc){
+function ccValidation(cc){
   
-//     if((/^\d{4}\-\d{4}\-\d{4}\-\d{4}$/).exec(cc)){
-//         console.log('true');
-//     } else(console.log('false'));
-// }
+    if((/^\d{4}\-\d{4}\-\d{4}\-\d{4}$/).exec(cc)){
+        console.log('true');
+    } else(console.log('false'));
+}
 
-// ccValidation("1234-5678-9012-3456");
+ccValidation("1234-5678-9012-3456");
 
 
 
 // //task6//
 
 
-// function checkEmail(mail){
+function checkEmail(mail){
 
-//     const checkExp = mail.match((/\@/g));
-//     const emailBody = mail.split(checkExp)[0];
-//     const emailEnd =  mail.split(checkExp)[1];
+    const checkExp = mail.match((/\@/g));
+    const emailBody = mail.split(checkExp)[0];
+    const emailEnd =  mail.split(checkExp)[1];
 
 
                 
-//     // console.log((/^\w+\-{0,1}/i).test(emailBody))
-//     // console.log((/\w\./i).test(emailEnd))
+    // console.log((/^\w+\-{0,1}/i).test(emailBody))
+    // console.log((/\w\./i).test(emailEnd))
 
-//     if((/^[A-Za-z0-9]+([-]?)\w*$/).test(emailBody) && (/\w\./i).test(emailEnd) && checkExp.length === 1){
-//         console.log('true')
-//     }   else console.log('false')
-// }
+    if((/^[A-Za-z0-9]+([-]?)\w*$/).test(emailBody) && (/\w\./i).test(emailEnd) && checkExp.length === 1){
+        console.log('true')
+    }   else console.log('false')
+}
 
-// checkEmail('my_mail@gmail.com');
-// checkEmail('#my_mail@gmail.com');
+checkEmail('my_mail@gmail.com');
+checkEmail('#my_mail@gmail.com');
 
 
 // //task7//
 
 
 function checkLogin(login){
-
-   
 
     if(login.match((/[\!\&\^\%\$\*\(\)\+\>\<\[\]\{\}\?\:\;\,\`\~\#\"\'\/\|]/g)) || login.length > 10) {
         console.log('false');
