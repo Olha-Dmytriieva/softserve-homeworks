9f85aab3880ddd1672f47b349e425ed0f9c9a84d
Olha-Dmytriieva <tarakanola@gmail.com>
2021-03-01 13:26:04 +0200

    hw10 done

--- softserve-homework-10/script.js
+++ softserve-homework-10/script.js
@@ -1,47 +1,112 @@
 
 //task1
 
-// let arr = ["Tom", "Sam", "Ray", "Bob"];
+let arr = ["Tom", "Sam", "Ray", "Bob"];
 
-// let [x, y, , ...z] = arr;
-// console.log(x); 
-// console.log(y); 
-// console.log(z); 
+let [x, y, , ...z] = arr;
+console.log(x); 
+console.log(y); 
+console.log(z); 
 
 //task2
 
-// let data = {
-//    names: ["Sam", "Tom", "Ray", "Bob"],
-//    ages: [20, 24, 22, 26],
-// };
-// let {names:[name1, name2, name3, name4], ages:[age1, age2, age3, age4]} = data;
+let data = {
+   names: ["Sam", "Tom", "Ray", "Bob"],
+   ages: [20, 24, 22, 26],
+};
+let {names:[name1, name2, name3, name4], ages:[age1, age2, age3, age4]} = data;
 
 
-// console.log(name2); // "Tom"
-// console.log(age2); // 24
-// console.log(name4); // "Bob"
-// console.log(age4); // 26
+console.log(name2); // "Tom"
+console.log(age2); // 24
+console.log(name4); // "Bob"
+console.log(age4); // 26
 
 
 //task3
 
-// Напишіть функцію mul(), яка приймає будь-яку кількість параметрів 
-// різного типу і повертає добуток параметрів типу Number.
-// Якщо параметри типу Number відсутні, повертає число 0.
-
-
-// Не можна використовувати властивість arguments, але в функцію mul() можна додати один параметр.
-
-
-
 function mul(...params) {
-    // Ваш код
-
-    console.log(params)
-
-
+ let total = 0;   
+    for(let i = 0; i < params.length; i+=1){
+     
+        if(typeof params[i] !== 'number' ){
+           params.splice(i, 1, 0)
+        }
+         total += params[i]
+    }
+    console.log(total)
 }
 
 
 mul(1, "str", 2, 3, true); // 6
 mul(null, "str", false, true); // 0
+
+
+//task4
+
+
+let server = {
+    data: 0,
+    convertToString: function (callback) {
+
+        callback(() => {
+            return this.data + "";
+        })
+    }
+ };
+ let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+       this.server.data = data;
+       this.server.convertToString(this.notification());
+    },
+
+    notification: function () {
+       return ( (callback) => {
+          this.result = callback();
+       });
+    }
+ };
+ client.calc(123);
+ console.log(client.result); // "123"
+ console.log(typeof client.result);
+
+
+
+
+
+
+
+//task5
+
+
+
+
+
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+
+const map1= new Map();
+let map = mapBuilder(keys, values)
+
+
+
+function mapBuilder (keysArray, valuesArrays){
+   
+
+    for(let i = 0; i < 4; i+=1){
+     
+        map1.set(keysArray[i], valuesArrays[i] )
+    }
+    
+ 
+
+    
+}
+
+// mapBuilder(map)
+
+console.log(map1)
+console.log(map1.size); // 4
+console.log(map1.get(2))
--- softserve-project/styles/script.js
+++ softserve-project/styles/script.js
@@ -1,128 +1,6 @@
 
-// const item = {
-//     form: document.querySelector('.js-form'),
-//     username: document.querySelector('.js-form input[name=username]'),
-//     password: document.querySelector('.js-form input[name=password]'),
-//     newpass: document.querySelector('.js-form input[name=new-password]')
-// }
-
-// console.log(item.username)
-
-// item.form.addEventListener('submit', handleFormSubmit)
-// item.username.addEventListener('input', handleInputUsername);
-// item.password.addEventListener('input', handleInputPassword);
-// item.newpass.addEventListener('input', handleInputNewPassword);
-
-
-// function handleFormSubmit(event){
-//     // event.preventDefault()
-//     console.log(event)
-// }
-
-// // const id = `f${(~~(Math.random()*1e8)).toString(16)}`
-
-// function handleInputUsername(event){
-//     console.log('event')
-//     const value = event.currentTarget.value;
-//    console.log(event)
-
-
-//     localStorage.setItem(`username`, value);
-
-//     // event.currentTarget.textContent = value
-
- 
-// }
-
-// const usernameFromLocalStorage = localStorage.getItem('username');
-    
-//         if (usernameFromLocalStorage){
-    
-//          item.username.value = usernameFromLocalStorage   
-
-//     } 
-
-// //==========current password============//
-
-// function handleInputPassword(event){
-//         const value = event.currentTarget.value;
-//         localStorage.setItem(`password-${id}`, value);
-     
-// } 
-
-
-// const passwordFromLocalStorage = localStorage.getItem('password');
-
-//         if (passwordFromLocalStorage){
-
-//             item.password.value = passwordFromLocalStorage
-    
-// } 
-
-// //===================update password==============//
-
-
-// function handleInputNewPassword(event){
-    
-//     const value = event.currentTarget.value;
-//     localStorage.setItem('password', value);
-   
-// }
-
-
-
-
-// const item = {
-//     form: document.querySelectorAll('.js-form'),
-//     username: document.querySelectorAll('.js-form input[name=username]'),
-//     password: document.querySelectorAll('.js-form input[name=password]'),
-//     newpass: document.querySelectorAll('.js-form input[name=new-password]')
-// }
-
-// console.log(item.username)
-
-// item.form.forEach((element => element.addEventListener('submit', handleFormSubmit)))
-// item.username.forEach((element => element.addEventListener('input', handleInputUsername)));
-
-
-// function handleFormSubmit(event){
-//     // event.preventDefault()
-//     // console.log(event)
-// }
-
-// // const id = `f${(~~(Math.random()*1e8)).toString(16)}`;
-
-// function handleInputUsername(event){
-//     console.log(event)
-//     const value1 = event.currentTarget.value;
-//     console.log(this)   
-//     localStorage.setItem(`name1`, event.currentTarget.value);
-    
-//     const usernameFromLocalStorage = localStorage.getItem(`name1`);
-//     console.log(usernameFromLocalStorage) 
-
-//     if (usernameFromLocalStorage){
-//         console.log(this.textContent)
-//     this.innerText += usernameFromLocalStorage;
-
-    
-
-//         }
-//     }
-
-
-// //========================
-// // const item = {
-// //     form: document.querySelector('.js-form'),
-// //     username: document.querySelector('.js-form input[name=username]'),
-// //     password: document.querySelector('.js-form input[name=password]'),
-// //     newpass: document.querySelector('.js-form input[name=new-password]')
-// // }
-
-
-
 const buttonCreationRef = document.getElementById('create');
-// console.log(buttonCreationRef)
+
 const mainContainerRef = document.querySelector('.main');
 const j = document.querySelector('.js-item');
 
@@ -161,33 +39,43 @@ removeBtnRef.addEventListener('click', removeBtnFunction);
 
 function removeBtnFunction(event){
 
-    // const domElementsFromStorage = localStorage.getItem('html'); 
-    
     const formToRemove = (event.target.parentNode.parentNode);
     formToRemove.remove()
 
     let html = mainContainerRef.outerHTML
     localStorage.setItem('html', html);
-// 
+
 }
 
 
-// const o = document.querySelector('div > value')
-// console.log(o)
 
 
 const item = {
     form: document.querySelectorAll('.js-form'),
+    name:document.querySelectorAll('.js-form input[name=name]'),
     username: document.querySelectorAll('.js-form input[name=username]'),
     password: document.querySelectorAll('.js-form input[name=password]'),
     newpass: document.querySelectorAll('.js-form input[name=new-password]')
 }
 
-// console.log(item.username)
 
 item.form.forEach((element => element.addEventListener('submit', handleFormSubmit)));
+
+item.name.forEach((element => element.addEventListener('input', handleInputName)));
+// item.name.forEach((element => element.addEventListener('onload', returnNameTextContent)));
+
+// window.addEventListener('click', function lala (e){
+// console.log(e.target)
+// } )
+
 item.username.forEach((element => element.addEventListener('input', handleInputUsername)));
 item.username.forEach((element => element.addEventListener('click', returnTextContent)));
+
+item.password.forEach((element => element.addEventListener('input', handlePasswordUsername)));
+item.password.forEach((element => element.addEventListener('click', returnPasswordTextContent)));
+
+item.newpass.forEach((element => element.addEventListener('input', handleNewPasswordUsername)));
+
 
 
 function handleFormSubmit(event){
@@ -195,7 +83,47 @@ function handleFormSubmit(event){
     // console.log(event)
 }
 
-// const id = `f${(~~(Math.random()*1e8)).toString(16)}`;
+
+
+
+
+function handleInputName(event){
+    const uniqueIDRef = (event.currentTarget.parentNode.parentNode.parentNode).getAttribute('value');
+      
+    localStorage.setItem(`name-${uniqueIDRef}`, event.currentTarget.value);
+    
+   
+}
+
+function returnNameTextContent(event){
+    const uniqueIDRef = (event.currentTarget.parentNode.parentNode.parentNode).getAttribute('value');
+    const nameFromLocalStorage = localStorage.getItem(`name-${uniqueIDRef}`);
+  
+        if (nameFromLocalStorage){
+  
+            event.currentTarget.placeholder = nameFromLocalStorage;
+
+
+}
+
+    }
+
+
+window.addEventListener('onload', function lala (e){
+
+    const nameFromLocalStorage = localStorage.getItem(`name-${uniqueIDRef}`);
+
+    if (nameFromLocalStorage){
+  
+        e.target.value = nameFromLocalStorage;
+}
+
+    // console.log(e.target)
+    } 
+    
+    )
+
+
 
 function handleInputUsername(event){
     const uniqueIDRef = (event.currentTarget.parentNode.parentNode.parentNode).getAttribute('value');
@@ -217,80 +145,37 @@ function returnTextContent(event){
 
 
 
-// const item = {
-//     form: document.querySelector('.js-form'),
-//     username: document.querySelector('.js-form input[name=username]'),
-//     password: document.querySelector('.js-form input[name=password]'),
-//     newpass: document.querySelector('.js-form input[name=new-password]')
-// }
 
-// console.log(item.username)
-
-// item.form.addEventListener('submit', handleFormSubmit)
-// item.username.addEventListener('input', handleInputUsername);
-// item.password.addEventListener('input', handleInputPassword);
-// item.newpass.addEventListener('input', handleInputNewPassword);
-
-
-// function handleFormSubmit(event){
-//     // event.preventDefault()
-//     console.log(event)
-// }
-
-// // const id = `f${(~~(Math.random()*1e8)).toString(16)}`
-
-// function handleInputUsername(event){
-//     console.log('event')
-//     const value = event.currentTarget.value;
-//    console.log(event)
-
-
-//     localStorage.setItem(`username`, value);
-
-//     // event.currentTarget.textContent = value
-
- 
-// }
-
-// const usernameFromLocalStorage = localStorage.getItem('username');
+function handlePasswordUsername(event){
+    const uniqueIDRef = (event.currentTarget.parentNode.parentNode.parentNode).getAttribute('value');
+      
+    localStorage.setItem(`password-${uniqueIDRef}`, event.currentTarget.value);
     
-//         if (usernameFromLocalStorage){
-    
-//          item.username.value = usernameFromLocalStorage   
-
-//     } 
-
-// //==========current password============//
-
-// function handleInputPassword(event){
-//         const value = event.currentTarget.value;
-//         localStorage.setItem(`password-${id}`, value);
-     
-// } 
-
-
-// const passwordFromLocalStorage = localStorage.getItem('password');
-
-//         if (passwordFromLocalStorage){
-
-//             item.password.value = passwordFromLocalStorage
-    
-// } 
-
-// //===================update password==============//
-
-
-// function handleInputNewPassword(event){
-    
-//     const value = event.currentTarget.value;
-//     localStorage.setItem('password', value);
    
-// }
+}
+
+
+function returnPasswordTextContent(event){
+    const uniqueIDRef = (event.currentTarget.parentNode.parentNode.parentNode).getAttribute('value');
+    const passwordFromLocalStorage = localStorage.getItem(`password-${uniqueIDRef}`);
+  
+        if (passwordFromLocalStorage){
+  
+        this.value = passwordFromLocalStorage;
+    }
+}
 
 
 
 
 
+function handleNewPasswordUsername(event){
+    const uniqueIDRef = (event.currentTarget.parentNode.parentNode.parentNode).getAttribute('value');
+      
+    localStorage.setItem(`password-${uniqueIDRef}`, event.currentTarget.value);
+    
+   
+}
 
 
 
